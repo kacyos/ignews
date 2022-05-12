d4a6f2b42db2a45eb1b4bd21cd935ab9c143f964
Cacio de Castro <cacio.castro@gmail.com>
2022-05-12 14:28:08 -0300

    integração com o banco de dados fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import { query as q } from "faunadb";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -12,4 +14,26 @@ export default NextAuth({
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
