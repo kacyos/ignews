bab9f7bfcb4586cb7d7d70e6e12e3a39e29b7f5f
Cacio de Castro <cacio.castro@gmail.com>
2022-08-09 15:11:15 -0300

    correções para deploy

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import { version } from "../../package.json";
 
 const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  // @ts-ignore stripe-version-2022-08-01
   apiVersion: "2020-08-27",
   appInfo: {
     name: "Ignews",
